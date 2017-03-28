371a98e969458c990e848e51a035fe4afd07985e
horejvi14 <horejvi14@DST20716>
2017-03-28 11:36:25 +0200

    neco

---
+++ Telefonní seznam SQLite/Telefonní_seznam_SQLite/db/IFileHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telefonní_seznam_SQLite
+{
+    public interface IFileHelper
+    {
+        string GetLocalFilePath(string filename);
+    }
+}
+
